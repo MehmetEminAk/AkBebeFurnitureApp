18ae91414aea6144378df9f1606bb73fb4daa3ad
Mehmet Emin Ak <mak44162@gmail.com>
2023-04-02 15:19:10 +0300

    Initial Commit

---
+++ MyApi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyApi
+//
+//  Created by Macbook Air on 2.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
